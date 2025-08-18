e1234626be02dc463c4ea573bc8c0f1925e8bcca
Dzhyan <lmchen83@gmail.com>
2025-08-18 19:00:48 +0100

    Admin page work

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")).render(
     clientId={import.meta.env.VITE_AUTH0_CLIENT_ID || import.meta.env.AUTH0_CLIENT_ID}
     
     authorizationParams={{
-      redirect_uri: window.location.origin + "/auth/callback",
+      redirect_uri: window.location.origin + "/admin",
     }}
   >
       <StrictMode>
